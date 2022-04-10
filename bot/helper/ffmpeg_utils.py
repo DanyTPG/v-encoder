af78074c8b86e97d5c0a60630215c4630c6f4182
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2022-04-11 03:17:10 +0430

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -36,7 +36,10 @@ def encode(filepath):
             video_opts = '-c:v copy -tag:v hvc1'
     else:
         # Transcode to h265 / hvc1
-        video_opts = '-c:v libx265 -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -crf 24.2  -threads 8  -pix_fmt yuv420p -preset slow -profile:v main -vf smartblur=1.5:-0.35:-3.5:0.65:0.25:2.0,scale=1280:720:spline16+accurate_rnd+full_chroma_int -x265-params me=2:rd=4:subme=7:aq-mode=3:aq-strength=1:deblock=1,1:psy-rd=1:psy-rdoq=1:rdoq-level=2:merange=32:bframes=8:b-adapt=2:limit-sao=1'
+        video_opts = '-c:v libx265 -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -crf 24.2  -threads 8  -pix_fmt yuv420p -preset slow -profile:v main -vf'
+        opt1 = ",".join(['smartblur=1.5:-0.35:-3.5:0.65:0.25:2.0','scale=1280:720:spline16+accurate_rnd+full_chroma_int'])
+        opt2 = '-x265-params'
+        opt3 = ":".join(['me=2','rd=4','subme=7','aq-mode=3','aq-strength=1','deblock=1,1','psy-rd=1','psy-rdoq=1','rdoq-level=2','merange=32','bframes=8','b-adapt=2','limit-sao=1'])
     # Get the audio channel codec
     audio_codec = get_codec(filepath, channel='a:0')
     if audio_codec == []:
@@ -45,7 +48,7 @@ def encode(filepath):
         audio_opts = '-c:a copy -c:s copy'
     else:
         audio_opts = '-c:a libopus -b:a 128k'
-    call(['ffmpeg', '-i', filepath] + video_opts.split() + audio_opts.split() + [output_filepath])
+    call(['ffmpeg', '-i', filepath] + video_opts.split() + opt1 + opt2 + opt3 + audio_opts.split() + [output_filepath])
     os.remove(filepath)
     return output_filepath
 
