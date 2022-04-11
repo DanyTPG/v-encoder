843ca32fe959ccf3e832ef4d98356908767a43fa
DanyTPG <50212295+DanyTPG@users.noreply.github.com>
2022-04-11 04:32:49 +0430

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -50,7 +50,7 @@ def encode(filepath):
         audio_opts = '-c:a libopus -b:a 128k'
     #call(['ffmpeg', '-i', filepath] + video_opts.split() + opt1.split() + opt2.split() + opt3.split() + audio_opts.split() + [output_filepath])
     #call(['ffmpeg', '-i', filepath, audio_opts, video_opts , opt1, opt2, opt3, output_filepath])
-    call(['ffmpeg', '-i', filepath, '-map 0:v -map 0:a -b:a 128k -c:a libopus -c:v libx265 -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -crf 24.2 -map 0:s? -c:s copy  -pix_fmt yuv420p -preset slow -profile:v "main" -vf smartblur=1.5:-0.35:-3.5:0.65:0.25:2.0,scale=1280:720:spline16+accurate_rnd+full_chroma_int -x265-params me=2:rd=4:subme=7:aq-mode=3:aq-strength=1:deblock=1,1:psy-rd=1:psy-rdoq=1:rdoq-level=2:merange=32:bframes=8:b-adapt=2:limit-sao=1', output_filepath])
+    call(['ffmpeg', '-i', filepath, '-b:a 128k -c:a libopus -c:v libx265 -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -crf 24.2  -threads 8 -c:s copy  -pix_fmt yuv420p -preset slow -profile:v main -vf smartblur=1.5:-0.35:-3.5:0.65:0.25:2.0,scale=1280:720:spline16+accurate_rnd+full_chroma_int -x265-params me=2:rd=4:subme=7:aq-mode=3:aq-strength=1:deblock=1,1:psy-rd=1:psy-rdoq=1:rdoq-level=2:merange=32:bframes=8:b-adapt=2:limit-sao=1', output_filepath])
     os.remove(filepath)
     return output_filepath
 
